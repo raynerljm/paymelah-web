9d1ca765121247804be9f3ebe4f35481a71febcf
raynerljm <raynerljm@gmail.com>
2022-01-07 19:10:19 +0800

    Fix

--- components/Telebot.tsx
+++ components/Telebot.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import Image from "next/image";
 import { Fade } from "react-awesome-reveal";
-import Button from "./Button";
+import Button from "./Forms/Button";
 
 const Telebot: FC = ({ children }) => {
   return (
@@ -14,48 +14,47 @@ const Telebot: FC = ({ children }) => {
       </p>
 
       <div className="space h-almost mx-7" id="home">
-      <div className=" grid grid-cols-2 h-4/5">
-        <div className="flex items-center justify-center h-full">
-          <div className="transform hover:scale-110 hover:-translate-y-16 transition-all">
-            <Fade direction="up">
-              <Image
-                src="/Telegram.png"
-                alt="Telegram Icon"
-                layout="fixed"
-                width={300}
-                height={300}
-              />
-            </Fade>
+        <div className=" grid grid-cols-2 h-4/5">
+          <div className="flex items-center justify-center h-full">
+            <div className="transform hover:scale-110 hover:-translate-y-16 transition-all">
+              <Fade direction="up">
+                <Image
+                  src="/Telegram.png"
+                  alt="Telegram Icon"
+                  layout="fixed"
+                  width={300}
+                  height={300}
+                />
+              </Fade>
+            </div>
+          </div>
+          <div className="flex justify-center items-center h-full">
+            <h1 className="text-white text-left text-3xl font-semibold">
+              <span className="font-semibold">
+                You can access the telegram bot here.
+              </span>
+              <br />
+              <br />
+              <span>
+                <Fade direction="up" delay={500}>
+                  <div className="ml-auto">
+                    <a
+                      href="https://t.me/PayMeLahBot"
+                      target="blank"
+                      rel="noreferrer"
+                    >
+                      <Button className="px-10 py-2 bg-white text-black font-bold max-w-max">
+                        Lets Go
+                      </Button>
+                    </a>
+                  </div>
+                </Fade>
+              </span>
+            </h1>
           </div>
         </div>
-        <div className="flex justify-center items-center h-full">
-          <h1 className="text-white text-left text-3xl font-semibold">
-            <span className="font-semibold">You can access the telegram bot here.</span>
-            <br/>
-            <br/>
-            <span>
-            <Fade direction="up" delay={500}>
-            <div className="ml-auto">
-                  <a
-                  href="https://t.me/PayMeLahBot"
-                  target="blank"
-                  rel="noreferrer"
-                >
-                    <Button
-                className="px-10 py-2 bg-white text-black font-bold max-w-max"
-              >Lets Go</Button>
-
-                </a>
-            </div>
-          </Fade>
-            </span>
-          </h1>
-        </div>
+        <div className="space"></div>
       </div>
-      <div className="space">
-          <p>""</p>
-      </div>
-    </div>
 
       <div className="space"></div>
     </div>
@@ -63,5 +62,3 @@ const Telebot: FC = ({ children }) => {
 };
 
 export default Telebot;
-
-
